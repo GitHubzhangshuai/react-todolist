b7274657a8f978d74c483995fd2c361a3a098fc7
GitHubzhangshuai <1802528291@qq.com>
2020-03-10 13:08:46 +0800

    redux-saga add try catch

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -4,9 +4,14 @@ import axios from 'axios'
 import {initAction} from './createActions'
 
 function* getInitList(){
-    const res = yield axios.get('http://localhost:1234/init')
-    const action = initAction(res.data)
-    yield put(action)
+    try{
+        const res = yield axios.get('http://localhost:1234/init')
+        const action = initAction(res.data)
+        yield put(action)
+    }
+    catch(e){
+        console.log(e)
+    }
 }
 
 function* sagas(){
