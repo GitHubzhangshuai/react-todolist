b9ed07b6337a13f84f554c6abf88bb432dacda2b
GitHubzhangshuai <1802528291@qq.com>
2020-03-10 17:02:32 +0800

    react-redux

--- src/Todolist2.js
+++ src/Todolist2.js
@@ -1,42 +1,22 @@
 import 'antd/dist/antd.css';
 import React, {Component} from 'react'
 import store from './store'
-import { changeAction, addAction, deleteAction, initAsyncAction } from './store/createActions';
 import Todolist2UI from './Todolist2UI'
+import {Provider} from 'react-redux'
+import {initAsyncAction} from './store/createActions'
 
 class Todolist2 extends Component {
-    constructor(props){
-        super(props)
-        console.log(store.getState())
-        this.state = store.getState()
-        this.handleChange = this.handleChange.bind(this)
-        this.handleStoreChange = this.handleStoreChange.bind(this)
-        this.handleBtn = this.handleBtn.bind(this)
-        store.subscribe(this.handleStoreChange)
-    }
+
     componentWillMount(){
         const action = initAsyncAction()
         store.dispatch(action)
     }
     render(){
         return (
-            <Todolist2UI state={this.state} handleBtn={this.handleBtn} handleChange={this.handleChange} handleDeleteItem={this.handleDeleteItem}></Todolist2UI>
+            <Provider store={store}>
+            <Todolist2UI></Todolist2UI>
+            </Provider>
         )
-    }
-    handleChange(e){
-        const action = changeAction(e.target.value)
-        store.dispatch(action)
-    }
-    handleBtn(){
-        const action = addAction()
-        store.dispatch(action)
-    }
-    handleDeleteItem(index){
-        const action = deleteAction(index)
-        store.dispatch(action)
-    }
-    handleStoreChange(){
-        this.setState(store.getState())
     }
 }
 
--- src/Todolist2UI.js
+++ src/Todolist2UI.js
@@ -1,21 +1,45 @@
 import React from 'react'
 import {Button, Input, List} from 'antd'
+import {connect} from 'react-redux'
+import { changeAction, addAction, deleteAction } from './store/createActions';
 
 const Todolist2UI = (props)  => {
         return (
-            <div style={{marginLeft: '10px',marginTop: '10px'}}>
-            <Input style={{width:'300px'}} placeholder="to do list" onChange={props.handleChange} value={props.state.inputValue}></Input>
-            <Button onClick={props.handleBtn}>提交</Button>
-            <List
-  size="small"
-  header={<div>Header</div>}
-  footer={<div>Footer</div>}
-  bordered
-  dataSource={props.state.list}
-  renderItem={(item,index) => <List.Item onClick={() => props.handleDeleteItem(index)}>{item}</List.Item>}
-/>
-        </div>
+            <div style={{marginLeft: '10px',marginTop: '10px'}} >
+                <Input style={{width:'300px'}} placeholder="to do list" onChange={props.handleChange} value={props.inputValue}></Input>
+                <Button onClick={props.handleBtn}>提交</Button>
+                <List
+                        size="small"
+                        header={<div>Header</div>}
+                        footer={<div>Footer</div>}
+                        bordered
+                        dataSource={props.list}
+                        renderItem={(item,index) => <List.Item onClick={() => props.handleDeleteItem(index)}>{item}</List.Item>}
+                        />
+                </div>
         )
 }
 
-export default Todolist2UI
+const mapStateToProps = (state) => {
+        return {
+                list: state.list,
+                inputValue: state.inputValue
+        }
+}
+const mapDispatchToProps = (dispatch) => {
+        return {
+                handleDeleteItem(index){
+                        const action = deleteAction(index)
+                        dispatch(action)
+                },
+                handleChange(e){
+                        const action = changeAction(e.target.value)
+                        dispatch(action)
+                },
+                handleBtn(){
+                        const action = addAction()
+                        dispatch(action)
+                }
+        }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Todolist2UI)
